95055a2f8d9d8d88df331e213db915948ed11117
michalpom <michalpom@gmail.com>
2020-07-06 19:20:39 +0200

    Stage 2/5 completed.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,11 +1,25 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+
+        char[] symbol = new char[9];
+
+        System.out.print("Enter cells: ");
+        String cells = scanner.nextLine();
+        symbol = cells.toCharArray();
+
+        System.out.println("---------");
+
+        System.out.println("| " + symbol[0] + " " +  symbol[1] + " " +  symbol[2] + " |");
+        System.out.println("| " + symbol[3] + " " +  symbol[4] + " " +  symbol[5] + " |");
+        System.out.println("| " + symbol[6] + " " +  symbol[7] + " " +  symbol[8] + " |");
+
+        System.out.println("---------");
     }
 }
